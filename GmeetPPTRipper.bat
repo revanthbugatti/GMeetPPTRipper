d320334401a9d30ead828682fe6067b7d9795fb6
Revanth Uppalapati <44273258+revanthbugatti@users.noreply.github.com>
2021-10-16 16:56:01 +0530

    Add files via upload
    Uses scdet filter from ffmpeg

--- GmeetPPTRipper.bat
+++ GmeetPPTRipper.bat
@@ -2,5 +2,5 @@
 set "PNGSlidesdir=%CD%"
 for %%f in (*.mp4) do (
   If not Exist "%PNGSlidesdir%\%%~nf" MkDir "%PNGSlidesdir%\%%~nf"
-  .\ffmpeg -i "%%f" -vf "crop=960:544:0:88,select=gt(scene\,0.04), scale=1280:720" -vsync vfr "%PNGSlidesdir%\%%~nf\%%~nf_%%d.png"
+  .\ffmpeg -i "%%f" -vf "crop=960:544:0:88,scdet=s=1:t=2.5, scale=1280:720" -vsync vfr "%PNGSlidesdir%\%%~nf\%%~nf_%%d.png"
 )
